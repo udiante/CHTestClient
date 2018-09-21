67629735a24b0c73d2fd211b0d1ac1d52e0cf9b8
Alejandro Quibus <udiante@gmail.com>
2018-09-21 23:46:54 +0200

    Initial Commit

---
+++ CHTestClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CHTestClient
+//
+//  Created by Alejandro Quibus on 21/9/18.
+//  Copyright © 2018 Alejandro Quibus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
